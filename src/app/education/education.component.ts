3a96c7c08e077941cd984e5cb510fdb47d48ebe4
Deepak Kanojia <kanojiadeepak401@gmail.com>
2025-02-24 21:28:13 +0000

    my resume

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -2,25 +2,25 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-education',
-  imports: [CommonModule],
-  templateUrl: './education.component.html',
-  styleUrl: './education.component.css'
+  selector: 'app-education', // Component selector
+  imports: [CommonModule], // Module dependencies
+  templateUrl: './education.component.html', // Template file
+  styleUrls: ['./education.component.css'] // Styles file
 })
 export class EducationComponent {
-
-  experiences = [
+  // Array of education details
+  education = [
     {
-      title: 'Software Engineer',
-      company: 'Tech Corp',
-      duration: 'Jan 2020 - Present',
-      description: 'Developed web applications using Angular and Node.js.'
+      degree: 'Bachelor of Science in Computer Science',
+      institution: 'University of XYZ',
+      duration: '2015 - 2019',
+      description: 'Graduated with honors. Relevant coursework: Data Structures, Algorithms, Web Development.'
     },
     {
-      title: 'Intern',
-      company: 'Startup Inc',
-      duration: 'Jun 2019 - Dec 2019',
-      description: 'Assisted in building REST APIs and debugging applications.'
+      degree: 'High School Diploma',
+      institution: 'ABC High School',
+      duration: '2011 - 2015',
+      description: 'Graduated with a focus on science and mathematics.'
     }
   ];
 }
